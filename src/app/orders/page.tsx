dab100d8e28f823ec624dba11181ef9f080f9b0a
nhan nguyen <resido.ken@gmail.com>
2025-12-27 20:35:53 +0700

    feat: create orders page to list user orders

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,6 +1,8 @@
 import { getOrders } from "./actions";
 import { OrdersClient } from "./orders-client";
 
+export const dynamic = 'force-dynamic';
+
 export default async function OrdersPage() {
     const orders = await getOrders();
 
