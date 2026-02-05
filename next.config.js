af7ec907b05b270c38608c131fa29cf51a5417ae
residoken-wq <resido.ken@gmail.com>
2026-02-05 15:22:35 +0700

    fix error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    transpilePackages: ["ckeditor5", "@ckeditor/ckeditor5-react"],
 }
 
 module.exports = nextConfig
